d9a97b914213555c7f65435362231ace54459fcd
Your Name <you@example.com>
2020-12-16 14:36:42 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'',component:LoginComponent
+  },
+  {
+    path:'register',component:RegisterComponent
+  },
+  {
+    path:'forgot-password',component:ForgotPasswordComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
